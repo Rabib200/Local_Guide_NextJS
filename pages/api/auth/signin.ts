65446ce11cd17309b7366090153e96be25e16ad5
Erin-Mrittika <erinmitti69@gmail.com>
2024-02-28 16:44:45 +0600

    middleware

--- pages/api/auth/signin.ts
+++ pages/api/auth/signin.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
+import { setCookie } from "cookies-next";
 import * as jose from "jose";
 import { NextApiRequest, NextApiResponse } from "next";
+import { redirect} from "next/navigation";
 import validator from "validator";
 
 const prisma = new PrismaClient();
@@ -35,32 +37,54 @@ export default async function handler(
     if (errors.length) return res.status(400).json({ errorMessage: errors[0] });
     console.log(errors);
 
-    const userWithEmail = await prisma.user.findUnique({
+    const user = await prisma.user.findUnique({
       where: {
         email,
       },
     });
-    if (!userWithEmail) {
-      return res.status(401).json({ errorMessage: "Email is invalid!!" });
+    if (!user) {
+      return res.status(401).json({ errorMessage: "Email or password is invalid!!" });
     }
 
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-    const isMatch = await bcrypt.compare(password, userWithEmail.password);
+    
 
-    if (isMatch) {
+    //const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+     return res.status(401).json({errorMessage: "Email or password is invalid!"});
+      }
+
       const alg = "HS256";
-      const token = await new jose.SignJWT({
-        email: userWithEmail.email,
-      })
-        .setProtectedHeader({ alg })
-        .setExpirationTime("24h")
-        .sign(secret);
-      console.log(token);
-      res.setHeader("Location", "/dest_loc");
-      res.statusCode = 302; // or 301 if you want a permanent redirect
-      res.end();
 
-      return res.status(200).json({ Hello: token });
-    } else return res.status(400).json({ Hello: "Error" });
-  }
-}
+      const secert = new TextEncoder().encode(process.env.JWT_SECRET);
+
+      const token = await new jose.SignJWT({email:user.email}).setProtectedHeader({alg}).setExpirationTime("24h").sign(secert);
+
+      setCookie("jwt",token,{req,res,maxAge: 60*6*24});
+      
+      redirect("/dest_loc");
+
+      // return res.status(200).json({
+      //   firstName: user.firstName,
+      //   lastName: user.lastName,
+      //   email:user.email,
+      //   phone:user.phone,
+      //   city:user.city,
+      }
+
+      //   .setProtectedHeader({ alg })
+      //   .setExpirationTime("24h")
+      //   .sign(secret);
+      // console.log(token);
+      // res.setHeader("Location", "/dest_loc");
+      // res.statusCode = 302; // or 301 if you want a permanent redirect
+      // res.end();
+    
+   
+    } 
+    
+    
+  //    return res.status(404).json( "Unkown Endpoint");
+  // }
+
